65ba115e6e7f6551c27bb8a4371e3761fdc5c8f6
Agbemuko David Ifeoluwa <davidagbemuko.ad@gmail.com>
2022-04-27 21:00:37 +0100

    added while loop. also made user experience chages.

--- Program.cs
+++ Program.cs
@@ -6,48 +6,59 @@ namespace Lab_3
         static void Main(string[] args)
         {
             // this program identifies a prime number.
-            Console.WriteLine("THIS PARTICULAR CONSOLE APPLICATION IDENTIFIES A PRIME NUMBER\n");
+            Console.WriteLine("\n"+
+                @"THIS PARTICULAR CONSOLE APPLICATION IDENTIFIES A PRIME NUMBER");
 
             //output_texts
             string True = "this IS A prime number";
             string False = "this IS NOT A prime number";
 
-            // take raw input from user
-            Console.Write("Please input in your integer (must be an integer): ");
-            string rawInput = Console.ReadLine();
-
-            //test input for correctness. INPUT MUST BE AN INTEGER TYPE
-            int cleanedInput;
-            bool testInput = int.TryParse(rawInput, out cleanedInput);
-
-            if (!testInput)
+            while (true)
             {
-                Console.WriteLine("INVALID INPUT!. (Input has to be an integer type!)");
-            }
-            else
-            {
-                if (cleanedInput==1)
+                // take raw input from user
+                Console.Write(@"
+                You can quit the program by typing 'quit'.
+                Please input in your integer (must be an integer): ");
+                string rawInput = Console.ReadLine();
+                if (rawInput == "quit")
                 {
-                    Console.WriteLine(False);
+                    break;
                 }
-                else if (cleanedInput==2)
+
+                //test input for correctness. INPUT MUST BE AN INTEGER TYPE
+                int cleanedInput;
+                bool testInput = int.TryParse(rawInput, out cleanedInput);
+
+                if (!testInput)
                 {
-                    Console.WriteLine(True);
+                    Console.WriteLine("\n" +
+                    @"INVALID INPUT!. (Input has to be an integer type!)");
                 }
                 else
                 {
-                    for (double i = 2; i < cleanedInput; i++)
+                    if (cleanedInput==1)
                     {
-                        if ((cleanedInput%i) == 0)
+                        Console.WriteLine(False);
+                    }
+                    else if (cleanedInput==2)
+                    {
+                        Console.WriteLine(True);
+                    }
+                    else
+                    {
+                        for (double i = 2; i < cleanedInput; i++)
                         {
-                            Console.WriteLine(False);
-                            break;
-                        }
-                        else
-                        {
-                            if (i == (cleanedInput - 1))
+                            if ((cleanedInput%i) == 0)
                             {
-                                Console.WriteLine(True);
+                                Console.WriteLine(False);
+                                break;
+                            }
+                            else
+                            {
+                                if (i == (cleanedInput - 1))
+                                {
+                                    Console.WriteLine(True);
+                                }
                             }
                         }
                     }
